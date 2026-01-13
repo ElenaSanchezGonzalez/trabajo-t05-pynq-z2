b0d877d2eefe5ec08b51b0240cad1bb021b49f90
Aleemanza <130283631+Aleemanza@users.noreply.github.com>
2026-01-13 19:28:44 +0100

    Update color_invert_hls.cpp

--- hls/color_invert_hls.cpp
+++ hls/color_invert_hls.cpp
@@ -7,19 +7,31 @@ void color_invert(axis_stream_t& in_stream, axis_stream_t& out_stream) {
 
     while (!in_stream.empty()) {
 #pragma HLS PIPELINE II=1
+
         axis_pixel_t in_px = in_stream.read();
 
-        ap_uint<24> rgb = in_px.data;
+        // TDATA de 32 bits
+        ap_uint<32> data_in = in_px.data;
 
-        ap_uint<8> r = rgb.range(23,16);
-        ap_uint<8> g = rgb.range(15,8);
-        ap_uint<8> b = rgb.range(7,0);
+        // RGB en los 24 bits menos significativos
+        ap_uint<8> r = data_in.range(23,16);
+        ap_uint<8> g = data_in.range(15,8);
+        ap_uint<8> b = data_in.range(7,0);
+
+        // Invertimos colores
+        ap_uint<8> r_inv = 255 - r;
+        ap_uint<8> g_inv = 255 - g;
+        ap_uint<8> b_inv = 255 - b;
 
         axis_pixel_t out_px = in_px;
-        out_px.data.range(23,16) = (ap_uint<8>)(255 - r);
-        out_px.data.range(15,8)  = (ap_uint<8>)(255 - g);
-        out_px.data.range(7,0)   = (ap_uint<8>)(255 - b);
+
+        // Reconstruimos TDATA de 32 bits
+        out_px.data = 0;
+        out_px.data.range(23,16) = r_inv;
+        out_px.data.range(15,8)  = g_inv;
+        out_px.data.range(7,0)   = b_inv;
 
         out_stream.write(out_px);
     }
 }
+
