6cda52e24ff9cb0ac418e99f98d496954b310b7a
Aleemanza <130283631+Aleemanza@users.noreply.github.com>
2026-01-13 19:23:11 +0100

    Update color_invert_hls.h

--- hls/color_invert_hls.h
+++ hls/color_invert_hls.h
@@ -3,5 +3,5 @@
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
-typedef ap_axiu<24,1,1,1> axis_pixel_t;
+typedef ap_axiu<32,1,1,1> axis_pixel_t;
 typedef hls::stream<axis_pixel_t> axis_stream_t;
